5795be0a4dfa48f105fbc9b87f6cb50eaf72e077
Kimidarious <kim.simoes78@hotmail.com>
2022-12-19 22:25:41 -0300

    const

---
+++ const.js
@@ -0,0 +1,20 @@
+//não podemos criar constantes com palavras reservadas
+//constantes precisam ter nomes significativos
+//não pode começar o nome de uma constante com um numero
+//não podem conter espaços ou traços
+/*
+ utilizamos CamelCase
+ Case-sensitive
+ não podemos modificar o valor de uma constante
+ NÃO UTILIZE VAR, UTILIZE CONST
+*/
+
+// const nome = "João";
+// nome = "Eliakim"; //não pode mudar o valor
+// console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2
+console.log(resultado);
